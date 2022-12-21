5e562c60913a12732dcbab37175ca462e5c221c4
KamauRuth <rkamau573@gmail.com>
2022-12-21 13:18:17 -0500

    if

---
+++ condition2.js
@@ -0,0 +1,8 @@
+var a=30;
+var b=10;
+if (a<10){
+	console.log("This is false");
+}
+else{
+	console.log("this is false");
+}
---
+++ condition3.js
@@ -0,0 +1,12 @@
+var a=10;
+var b=70;
+var c=50;
+if (a<b&&c>b){
+	console.log("maximum value is:" +b);
+}
+else if(c<b||c>a){
+	console.log("maximum value is:"+c)
+}
+else {
+	console.log("false")
+}
